6d448c1b97bf5bbfb30c6662c95913df4218d368
Cameron Fiorato <cameronclayfiorato@gmail.com>
2022-01-04 17:06:34 -0600

    added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,22 @@
 
-
 function Header() {
   return(
-    <h1>first.fm (header here)</h1>
+    <header>
+      <img src="https://media.istockphoto.com/vectors/headset-icon-music-templates-vector-id1187826963?k=20&m=1187826963&s=612x612&w=0&h=RwruPmaxyRiNzwxHtllG_Omzwa8ky07EOqR95ZRwWbU=" alt="first.fm"/>
+      <h1>first.fm</h1>
+      <nav>
+        <a className="button" href="/">
+          Home 🎧
+        </a>
+        <a className="button" href="/Library">
+          Library 🎧
+        </a>
+        <a className="button" href="/Playlist">
+          Playlist 🎧
+        </a>
+      </nav>
+    </header>
+    
   )
 }
 
